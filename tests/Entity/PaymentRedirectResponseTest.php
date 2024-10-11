97b37a0ab40150ef1912a6d77c0a2eb0543eb5cd
Anatoliy Legkiy <anatoliy.legkiy@paysera.net>
2024-10-11 15:35:50 +0300

    add test for PaymentRedirectResponse::getData

--- tests/Entity/PaymentRedirectResponseTest.php
+++ tests/Entity/PaymentRedirectResponseTest.php
@@ -10,12 +10,21 @@ use Paysera\CheckoutSdk\Tests\AbstractCase;
 class PaymentRedirectResponseTest extends AbstractCase
 {
     protected const TEST_URL = 'http://example.paysera.test';
+    protected const TEST_DATA = 'qweertrtytyutyu';
 
     public function testGetRedirectUrl(): void
     {
         $this->assertEquals(
             self::TEST_URL,
-            (new PaymentRedirectResponse(self::TEST_URL))->getRedirectUrl()
+            (new PaymentRedirectResponse(self::TEST_URL, self::TEST_DATA))->getRedirectUrl()
+        );
+    }
+
+    public function testGetData(): void
+    {
+        $this->assertEquals(
+            self::TEST_DATA,
+            (new PaymentRedirectResponse(self::TEST_URL, self::TEST_DATA))->getData()
         );
     }
 }
